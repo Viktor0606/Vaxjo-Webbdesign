418f6241589697b9dd436125383d79f889a2c9ea
Viktor0606 <viktor.ylven@gmail.com>
2024-07-26 14:13:55 +0200

    nav och hamburger

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerButton = document.querySelector('.hamburger-button');
-    const mobileMenu = document.querySelector('.mobile');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile");
 
-    hamburgerButton.addEventListener('click',  () => mobileMenu.classList.toggle('active'))
-})
+  hamburgerButton.addEventListener("click", () =>
+    mobileMenu.classList.toggle("active")
+  );
+  hamburgerButton.addEventListener("click", () =>
+    hamburgerButton.classList.toggle("active")
+  );
+});
