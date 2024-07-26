885ef741fd1d6b6aa626baa9747d4817484027aa
Viktor0606 <viktor.ylven@gmail.com>
2024-07-26 17:07:38 +0200

    hero

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburgerButton = document.querySelector(".hamburger-button");
   const mobileMenu = document.querySelector(".mobile");
+  const body = document.querySelector("body");
 
   hamburgerButton.addEventListener("click", () =>
     mobileMenu.classList.toggle("active")
   );
   hamburgerButton.addEventListener("click", () =>
     hamburgerButton.classList.toggle("active")
+  );
+  hamburgerButton.addEventListener("click", () =>
+    body.classList.toggle("stop-scrolling")
   );
 });
